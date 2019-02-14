8241360cece2778aa6aaa036d208535bc059dba8
Victoria Fuenmayor <vfuenmay@thoughtworks.com>
2019-02-14 19:15:16 -0300

    [@vicky/@julia] Adds first test crap

---
+++ UnityTest/Assets/Scripts/Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Julia {
+    public class Script : MonoBehaviour
+    {
+        public string getVicky()
+        {
+            return "Vicky";
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+
+    }
+}
---
+++ UnityTest/Assets/Tests/TesteDaVicky.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Julia;
+
+namespace Tests
+{
+    public class TesteDaVicky
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TesteDaVickySimplePasses()
+        {
+            var julia = new Julia.Script();
+            Equals(julia.getVicky(), "Vicky");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TesteDaVickyWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        public bool IsTestFinished
+        {
+            get { return true; }
+        }
+    }
+}
